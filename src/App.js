a07b5e89b0c39caf41efd2a3c2e93facc4de32b0
jhersbo <jhersbo@gmail.com>
2022-01-23 22:16:56 -0600

    just need to do the store listings

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import TopBar from './components/TopBar';
+import UpperSection from './components/UpperSection';
 
 function App() {
   return (
     <div>
       <TopBar></TopBar>
+      <UpperSection></UpperSection>
     </div>
   );
 }
--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -8,20 +8,22 @@ import {
 } from 'react-bootstrap'
 import NavbarCollapse from "react-bootstrap/esm/NavbarCollapse"
 import NavbarToggle from "react-bootstrap/esm/NavbarToggle"
+import Logo from '../2560px-Instacart_logo.png'
+import '../App.css'
 
 
 function TopBar(){
     return(
         <Navbar bg="light" expand="lg">
-            <NavbarBrand>
-                <img src="../2560px-Instacart_logo.png" alt="logo"></img>
+            <NavbarBrand style={{'margin': '5px 0 0 20px'}} href="home">
+                <img src={Logo} alt="Logo" style={{'width': '10%', 'height': 'auto'}}></img>
             </NavbarBrand>
             <NavbarToggle aria-controls="basic-navbar-nav"></NavbarToggle>
-            <NavbarCollapse id="basic-navbar-nav">
+            <NavbarCollapse id="basic-navbar-nav" style={{'margin': '5px 30px 5px 20px', 'textAlign': 'center', alignSelf: 'end'}}>
                 <Nav className="mr-auto"></Nav>
                 <Nav>
-                    <NavLink href='#LogIn'>Log In</NavLink>
-                    <Button variant="success">Sign Up</Button>
+                    <NavLink href='#LogIn' style={{'padding': '10px 10px', 'margin': '0px 10px', whiteSpace: 'nowrap'}}>Log In</NavLink>
+                    <Button variant="success" style={{'padding': '10px 10px', 'backgroundColor': '#28A845', 'border': 'none', whiteSpace: 'nowrap'}}>Sign Up</Button>
                 </Nav>
             </NavbarCollapse>
         </Navbar>
